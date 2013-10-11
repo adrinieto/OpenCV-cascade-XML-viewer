0d2bf95c477245aa4ee62582b84515397aeee153
Adrián Nieto <adrian.nieto7@gmail.com>
2013-10-11 13:07:39 +0200

    Now works with old version cascade. Readme updated.

--- src/cascade.py
+++ src/cascade.py
@@ -2,17 +2,10 @@
 
 import xml.etree.ElementTree as ET
 import sys
-import cv,cv2
+import cv, cv2
 
 class Rect:
-
-    def __init__(self, x, y, width, height, weight):
-        self._x = x
-        self._y = y
-        self._width = width
-        self._height = height
-        self._weight = weight
-        
+  
     def __init__(self, values):
         self._x = values[0]
         self._y = values[1]
@@ -50,22 +43,17 @@ class Cascade:
         height, width, _ = self._img.shape
         if width != self._params['width'] or height != self._params['height']:
             print "Cascade and image dimensions are different"
-            sys.exit(-1)
-          
-    def readFile(self, cascadeFile):
-        tree = ET.parse(cascadeFile)
-        #New version of cascade XML
-        root = tree.getroot()    #<opencv_storage>
-        cascade = root[0]        #<cascade_name> 
+            sys.exit()
 
+    def readNewStyle(self, cascadeNode):
         self._params = {}
-        self._params['width'] = int(cascade.find('width').text)
-        self._params['height'] = int(cascade.find('height').text)
-        self._params['stageNum'] = int(cascade.find('stageNum').text)
-
+        self._params['width'] = int(cascadeNode.find('width').text)
+        self._params['height'] = int(cascadeNode.find('height').text)
+        self._params['stageNum'] = int(cascadeNode.find('stageNum').text)
+    
         #Getting all features 
         self._features = []
-        feats = [feat for feat in cascade.find('features')]
+        feats = [feat for feat in cascadeNode.find('features')]
         for feat in feats:  
             rectList = []  
             for rect in feat.find('rects'):         
@@ -76,7 +64,7 @@ class Cascade:
             
         #Getting features for each stage
         self._stages = []
-        stages = [stage for stage in cascade.find('stages')]
+        stages = [stage for stage in cascadeNode.find('stages')]
         for stage in stages:
             classifiers = stage.find('weakClassifiers')
             features = []
@@ -85,7 +73,41 @@ class Cascade:
                 features.append(self._features[featId])
             self._stages.append(features)
         #print self._stages
+        
+    def readOldStyle(self, cascadeNode):
+        size = cascadeNode.find('size').text.split()
+        self._params = {}
+        self._params['width'] = int(size[0])
+        self._params['height'] = int(size[1])
+        
+        #Getting features for each stage
+        self._features = []
+        self._stages = []
+        stages = [stage for stage in cascadeNode.find('stages')]
+        for stage in stages:
+            features = []
+            for cl in stage.find('trees'):
+                rectList = []  
+                for rect in cl[0][0].find('rects'):  #_/feature/rects
+                    values = [int(x.replace('.','')) for x in rect.text.split()]
+                    rect = Rect(values)
+                    rectList.append(rect)
+                self._features.append(Feature(rectList))
+                features.append(self._features[len(self._features)-1])
+            self._stages.append(features)
             
+        self._params['stageNum'] = len(self._stages)
+        self._params['featuresNum'] = len(self._features)
+    
+    def readFile(self, cascadeFile):
+        tree = ET.parse(cascadeFile)
+        root = tree.getroot()    #<opencv_storage>
+        cascade = root[0]        #<cascade_name> 
+        if cascade.find('features') is None:
+            self.readOldStyle(cascade)
+        else:
+            self.readNewStyle(cascade)
+
     def getFeatures(self):
         return self._features
         
@@ -93,8 +115,8 @@ class Cascade:
         return self._stages
         
     def getParams(self):
-    	return self._params
-    	
+        return self._params
+    
     def getImage(self):
         return self._img.copy()
     
